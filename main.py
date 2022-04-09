4948ca59b0bf5b92978c1ee47eadacb3807fc24d
shir.zituni <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>
2022-04-09 16:16:53 +0300

    wrap around and fix movment

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def show(matrix):
     cv2.namedWindow("output", cv2.WINDOW_NORMAL)  # Create window with freedom of dimensions
     resized = cv2.resize(matrix.astype(np.uint8), (3000, 3000))  # Resize image
     cv2.imshow("output", resized)  # Show image
-    cv2.waitKey(300)
+    cv2.waitKey(10)
 
 
 def init_matrix():
@@ -48,40 +48,59 @@ def paint_cell_blue(matrix, pair):
     matrix[pair[0], pair[1]] = BLUE
 
 
+def paint_cell_black(matrix, pair):
+    matrix[pair[0], pair[1]] = BLACK
+
+
+def wrap_around(next_cell):
+
+    # check 4 corners
+    # top left
+    if next_cell == (-1, -1):
+        return 199, 199
+    # top right
+    elif next_cell == (200, -1):
+        return 0, 199
+    # down left
+    elif next_cell == (-1, 200):
+        return 199, 0
+    # down right
+    elif next_cell == (200, 200):
+        return 0, 0
+
+    # right side of matrix
+    elif next_cell[0] > 199:
+        return 0, next_cell[1]
+    # left side of matrix
+    elif next_cell[0] < 0:
+        return 199, next_cell[1]
+    # upside of matrix
+    elif next_cell[1] < 0:
+        return next_cell[0], 199
+    # downside of matrix
+    elif next_cell[1] > 199:
+        return next_cell[0], 0
+    else:
+        return next_cell
+
+
 def init():
-    while True:
-        # with_corona = int(N * D / 100)
-        matrix = init_matrix()
-        list_of_pairs = create_creatures()
-        for pair in list_of_pairs:
-            paint_cell_blue(matrix, pair)
+    # with_corona = int(N * D / 100)
+    matrix = init_matrix()
+    list_of_pairs = create_creatures()
+    for pair in list_of_pairs:
+        paint_cell_blue(matrix, pair)
 
-        # taking a random subgroup of N, it is our D
-        corona_list = random.sample(list_of_pairs, k=math.floor(D * N))
-        # painting in pink the corona sick
-        for pair in corona_list:
-            paint_cell_pink(matrix, pair)
+    # taking a random subgroup of N, it is our D
+    corona_list = random.sample(list_of_pairs, k=math.floor(D * N))
+    # painting in pink the corona sick
+    for pair in corona_list:
+        paint_cell_pink(matrix, pair)
 
-        # calculate the rest that doesn't have corona ( noCoronaList = listOfPairs - coronaList )
-        no_corona_list = list(set(list_of_pairs).difference(set(corona_list)))
+    # calculate the rest that doesn't have corona ( noCoronaList = listOfPairs - coronaList )
+    no_corona_list = list(set(list_of_pairs).difference(set(corona_list)))
 
-        # creating the movement for those who don't have corona (the blue points)
-        for pair in no_corona_list:
-            next_cell = move_cell(pair[0], pair[1])
-            # if the next move isn't to stay in place
-            if not compare(next_cell, pair):
-                # moving the next generation - those who don't have corona
-                matrix[pair[0], pair[1]] = BLACK  # paint the current cell in black
-                paint_cell_blue(matrix, next_cell)
-
-        for pair in corona_list:
-            next_cell = move_cell(pair[0], pair[1])
-            if not compare(next_cell, pair):
-                # moving the next generation - those who have corona
-                matrix[pair[0], pair[1]] = (0, 0, 0)
-                # next_cell = PINK
-
-        show(matrix)
+    return matrix, corona_list, no_corona_list
 
 
 def compare(pair, pair2):
@@ -98,8 +117,46 @@ def move_cell(x, y):
     return neighbourhoods_list[random_num]
 
 
+def is_cell_empty(matrix, next_cell):
+    if matrix[next_cell[0], next_cell[1]] is not BLACK:
+        return True
+    return False
+
+
+def steps(matrix, corona_list, no_corona_list):
+    while True:
+        new_no_corona_list = []
+        new_corona_list = []
+        # creating the movement for those who don't have corona (the blue points)
+        for pair in no_corona_list:
+            next_cell = move_cell(pair[0], pair[1])
+            # if the next_cell is outside the borders, Wrap around
+            next_cell = wrap_around(next_cell)
+            new_no_corona_list.append(next_cell)
+            # if the next move isn't to stay and the cell is empty
+            if not compare(next_cell, pair) and is_cell_empty(matrix, next_cell):
+                # moving the next generation - those who don't have corona
+                paint_cell_black(matrix, pair)  # paint the current cell in black
+                paint_cell_blue(matrix, next_cell)
+
+        # creating the movement for those who have corona (the pink points)
+        for pair in corona_list:
+            next_cell = move_cell(pair[0], pair[1])
+            # if the next_cell is outside the borders, Wrap around
+            next_cell = wrap_around(next_cell)
+            new_corona_list.append(next_cell)
+            if not compare(next_cell, pair) and is_cell_empty(matrix, next_cell):
+                # moving the next generation - those who have corona
+                paint_cell_black(matrix, pair)
+                paint_cell_pink(matrix, next_cell)
+        corona_list = new_corona_list
+        no_corona_list = new_no_corona_list
+        show(matrix)
+
+
 def main():
-    init()
+    matrix, corona_list, no_corona_list = init()
+    steps(matrix, corona_list, no_corona_list)
 
 
 if __name__ == "__main__":

c07644429ad22ea4a09444e37e2c2be6f3b445d8
Sapir Hender <sapirhender@gmail.com>
2022-04-14 23:46:27 +0300

    Sapir: organize project for exe

---
+++ src/consts.py
@@ -0,0 +1,18 @@
+SIZE = 200
+
+BLUE = (255, 0, 0)
+PINK = (238, 130, 238)
+BLACK = (0, 0, 0)
+GRAY = (220, 220, 220)
+
+BACKGROUND_IMAGES = [
+    'images/corona1.jpg',
+    'images/corona2.jpg',
+    'images/corona3.jpg',
+    'images/corona4.jpg',
+    'images/corona5.jpg',
+    'images/corona6.jpg',
+    'images/corona7.jpg',
+    'images/corona8.jpg',
+    'images/corona.jpg'
+]
